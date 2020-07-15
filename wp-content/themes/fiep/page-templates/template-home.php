5734cdb897fe5e4d6a6092befef5f06d7c18b16f
emilrichardo <emilrichardo@gmail.com>
2020-07-15 13:30:04 -0300

    quitar personas de home

--- wp-content/themes/fiep/page-templates/template-home.php
+++ wp-content/themes/fiep/page-templates/template-home.php
@@ -20,12 +20,12 @@
 
 
 
-<?php  get_template_part( 'template-parts/content/comite', sinatra_get_article_feed_layout() ); ?>
-<div class="mt-0 text-center bg-dark pb-5">
+<?php  //get_template_part( 'template-parts/content/comite', sinatra_get_article_feed_layout() ); ?>
+<!-- <div class="mt-0 text-center bg-dark pb-5">
 	<hr>
        
     <a href="/acerca-de-nosotros/#representantes" class="btn btn-outline-light btn-lg">Representantes Provinciales</a>
-</div>
+</div> -->
 
 <!-- cursos -->
 <?php  get_template_part( 'template-parts/content/cursos-home', sinatra_get_article_feed_layout() ); ?>	
