6389b710114cd554c6362de8d575ef0a791c35fa
emilrichardo <emilrichardo@gmail.com>
2020-07-17 17:39:52 -0300

    Search category

--- wp-content/themes/fiep/searchform.php
+++ wp-content/themes/fiep/searchform.php
@@ -17,9 +17,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 // Support for custom search post type.
 $sinatra_post_type = apply_filters( 'sinatra_search_post_type', 'all' );
 $sinatra_post_type = 'all' !== $sinatra_post_type ? '<input type="hidden" name="post_type" value="' . esc_attr( $sinatra_post_type ) . '" />' : '';
+$categoria = get_query_var('category_name' );
+
+
+
 ?>
 
-<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( 'categoria/'.$categoria.'/' ) ); ?>">
 	<div>
 		<span class="screen-reader-text"><?php esc_html_e( 'Buscar por:', 'sinatra' ); ?></span>
 		<input type="search" class="search-field" placeholder="<?php esc_attr_e( 'Buscar', 'sinatra' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
@@ -30,3 +34,20 @@ $sinatra_post_type = 'all' !== $sinatra_post_type ? '<input type="hidden" name="
 		</button>
 	</div>
 </form>
+
+<?php
+	
+    $term = get_queried_object();
+    $children = get_terms( $term->taxonomy, array(
+        'parent'    => $term->term_id,
+        'hide_empty' => false
+    ) );
+
+    if ( $children ) { 
+		echo '<hr>';
+        foreach( $children as $subcat )
+        {
+            echo '<a href="' . esc_url(get_term_link($subcat, $subcat->taxonomy)) . '">' . $subcat->name . '</a>';
+        }
+    }
+?>
