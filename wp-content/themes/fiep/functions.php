16f40080c78ead10054317e5fdcf48c8a5aa2a41
Nacho Fincatti <ignacio_f22@hotmail.com>
2020-08-24 09:15:30 -0300

    modificacion
    se agrego media.php dentro de tema fiep/format para poder quitar las clases de un div de la imagen para los cursos, se comento la funcion que cambia el estado de los pagos procesados a completados, hasta terminar el mail con link del aula virtual

--- wp-content/themes/fiep/functions.php
+++ wp-content/themes/fiep/functions.php
@@ -400,18 +400,18 @@ function my_pre_get_posts( $query ) {
 }
 
 // Actualiza automáticamente el estado de los pedidos a COMPLETADO
-add_action( 'woocommerce_order_status_processing', 'actualiza_estado_pedidos_a_completado' );
-// add_action( 'woocommerce_order_status_on-hold', 'actualiza_estado_pedidos_a_completado' );
-function actualiza_estado_pedidos_a_completado( $order_id ) {
-    global $woocommerce;
+// add_action( 'woocommerce_order_status_processing', 'actualiza_estado_pedidos_a_completado' );
+// // add_action( 'woocommerce_order_status_on-hold', 'actualiza_estado_pedidos_a_completado' );
+// function actualiza_estado_pedidos_a_completado( $order_id ) {
+//     global $woocommerce;
     
-    //ID's de las pasarelas de pago a las que afecta
-    // $paymentMethods = array( 'bacs', 'cheque', 'cod', 'paypal' );
-    $paymentMethods = array( 'woo-mercado-pago-basic' );
+//     //ID's de las pasarelas de pago a las que afecta
+//     // $paymentMethods = array( 'bacs', 'cheque', 'cod', 'paypal' );
+//     $paymentMethods = array( 'woo-mercado-pago-basic' );
     
-    if ( !$order_id ) return;
-    $order = new WC_Order( $order_id );
+//     if ( !$order_id ) return;
+//     $order = new WC_Order( $order_id );
 
-    if ( !in_array( $order->payment_method, $paymentMethods ) ) return;
-    $order->update_status( 'completed' );
-}
+//     if ( !in_array( $order->payment_method, $paymentMethods ) ) return;
+//     $order->update_status( 'completed' );
+// }
