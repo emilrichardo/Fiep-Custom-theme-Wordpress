48b163b869547e94edc9cfa4fd8f29f22c42982b
Nacho Fincatti <ignacio_f22@hotmail.com>
2020-09-22 16:46:42 -0300

    update
    modificacion en la validacion del chekout, validacion de usuarios al momento de inscribirse en un curso, no deja avanzar si ya esta inscripto

--- wp-content/themes/fiep/assets/js/checkout-validation.js
+++ wp-content/themes/fiep/assets/js/checkout-validation.js
@@ -3,11 +3,16 @@ jQuery(function($) {
     $('body').on('blur change', '#billing_first_name', function() {
         var wrapper = $(this).closest('.form-row');
         // you do not have to removeClass() because Woo do it in checkout.js
-        if (/\d/.test($(this).val())) { // check if contains numbers
+        if ($(this).val() == '') {
             wrapper.addClass('woocommerce-invalid'); // error
             wrapper.removeClass('woocommerce-validated'); // error
         } else {
-            wrapper.addClass('woocommerce-validated'); // success
+            if (/\d/.test($(this).val())) { // check if contains numbers
+                wrapper.addClass('woocommerce-invalid'); // error
+                wrapper.removeClass('woocommerce-validated'); // error
+            } else {
+                wrapper.addClass('woocommerce-validated'); // success
+            }
         }
     });
 
@@ -15,27 +20,31 @@ jQuery(function($) {
     $('body').on('blur change', '#billing_last_name', function() {
         var wrapper = $(this).closest('.form-row');
         // you do not have to removeClass() because Woo do it in checkout.js
-        if (/\d/.test($(this).val())) { // check if contains numbers
+        if ($(this).val() == '') {
             wrapper.addClass('woocommerce-invalid'); // error
             wrapper.removeClass('woocommerce-validated'); // error
         } else {
-
-            wrapper.addClass('woocommerce-validated'); // success
+            if (/\d/.test($(this).val())) { // check if contains numbers
+                wrapper.addClass('woocommerce-invalid'); // error
+                wrapper.removeClass('woocommerce-validated'); // error
+            } else {
+                wrapper.addClass('woocommerce-validated'); // success
+            }
         }
     });
 
 
     //Documento
-    $('body').on('blur change', '#billing_document', function() {
-        var wrapper = $(this).closest('.form-row');
-        // you do not have to removeClass() because Woo do it in checkout.js
-        if (/^([0-9])*$/.test($(this).val())) { // check if contains numbers
-            wrapper.addClass('woocommerce-validated'); // success
-        } else {
-            wrapper.addClass('woocommerce-invalid'); // error
-            wrapper.removeClass('woocommerce-validated'); // error
-        }
-    });
+    // $('body').on('blur change', '#billing_document', function() {
+    //     var wrapper = $(this).closest('.form-row');
+    //     // you do not have to removeClass() because Woo do it in checkout.js
+    //     if (/^([0-9])*$/.test($(this).val())) { // check if contains numbers
+    //         wrapper.addClass('woocommerce-validated'); // success
+    //     } else {
+    //         wrapper.addClass('woocommerce-invalid'); // error
+    //         wrapper.removeClass('woocommerce-validated'); // error
+    //     }
+    // });
 
     //Telefono
     // $('body').on('blur change', '#billing_phone', function() {
